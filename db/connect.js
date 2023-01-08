2b1bdfaaf1536c4188c58a740c83baa8a597263b
Monthon Nong <carrynong2@gmail.com>
2023-01-08 14:01:55 +0700

    refactor code with dotenv

--- db/connect.js
+++ db/connect.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://carrynong:0639042389zx@nodeexpressprojects.ddkhve9.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority";
+const connectDB = (url) => {
+  return mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
+};
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("CONNECTED TO THE DB..."))
-  .catch((err) => console.log(err));
+module.exports = connectDB;
